2034edaff9332377848df67d869862e5621e6b0f
harshalitalele <harshalitalele@gmail.com>
2018-03-26 18:02:08 +0530

    [Update] position of navigator fixed

--- draggable-navigator.js
+++ draggable-navigator.js
@@ -11,39 +11,46 @@
     9. Make it work for default 'isSet' value
     10. Add JavaScript docs
     11. Review the code and naming conventions
+    12. Don't fade navigator if user is dragging it
 */
 /******************************************************************************************/
 
 (function($) {
     "use strict";
-    
+
     //To Do: Test with different versions of OSD
     /*if (!$.version || $.version.major < 2) {
         throw new Error('This version of OpenSeadragonZoomLevels requires OpenSeadragon version 2.0.0+');
     }*/
-    
+
     $.Viewer.prototype.setNavigatorDraggable = function(isSet) {
         var droppableElem = this.element;
         var navigatorParent = this.navigator.container.parentElement.parentElement;
+        var draggableDivSize = 25;
         if(isSet) {
             if(this.navigator._draggable) {
-               return; 
+                return;
             } else {
                 var draggableDiv = document.createElement( "div" );
                 //To Do: Styling of draggable element
                 //To Do: Add a proper icon for draggable element
-                draggableDiv.style.width = "50px";
-                draggableDiv.style.height = "50px";
-                draggableDiv.style.backgroundColor = "blue";
+                draggableDiv.style.width = draggableDivSize + "px";
+                draggableDiv.style.height = draggableDivSize + "px";
+                draggableDiv.style.backgroundColor = "black";
                 draggableDiv.style.zIndex = 99999;
-                draggableDiv.style.color = "yellow";
+                draggableDiv.style.color = "white";
+                draggableDiv.style.textAlign = "center";
+                draggableDiv.style.fontWeight = "bolder";
                 draggableDiv.textContent = "+";
+
                 draggableDiv.addEventListener("drag", function(event) {
                     //To Do: See if commented code is required
                     //var elem = event.srcElement.parentElement.parentElement;
                     var elem = navigatorParent.parentElement;
-                    elem.style.top = event.y + "px";
-                    elem.style.left = event.x + "px";
+                    var osdy = droppableElem.getBoundingClientRect().y;
+                    var osdx = droppableElem.getBoundingClientRect().x;
+                    elem.style.top = event.y - osdy - navigatorParent.scrollHeight + "px";
+                    elem.style.left = event.x - osdx + "px";
                     elem.style.zIndex = 99999;
                 });
                 draggableDiv.setAttribute("draggable", "true");
